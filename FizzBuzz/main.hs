f222cc96047166a67632bb8169ac325df2cb8dcb
Kurt Logan <kurt.logan@digital.hmrc.gov.uk>
2017-05-09 16:52:28 +0100

    Refactored into Maybes and orElse's

--- FizzBuzz/main.hs
+++ FizzBuzz/main.hs
@@ -1,12 +1,32 @@
 module App where
 
-main = mapM_ putStrLn (map fizzleMaBizzle [1..100])
+import Data.Generics.Aliases
+
+main = mapM putStrLn (map fizzleMaBizzle [1..100])
 
 fizzleMaBizzle :: Int -> String
-fizzleMaBizzle x = fizzBuzz x
-fizzBuzz x = if x `divisable` 15 then "fizzbuzz" else fizz x
-fizz x = if x `divisable` 3 then "fizz" else buzz x
-buzz x = if x `divisable` 5 then "buzz" else show x
+fizzleMaBizzle x = unwrap (fizzBuzz x `orElse` fizz x `orElse` buzz x `orElse` none x)
+
+fizz :: Int -> Maybe String
+fizz x = case x `divisable` 3 of
+            True  -> Just "fizz"
+            False -> Nothing``
+
+buzz :: Int -> Maybe String
+buzz x = case x `divisable` 5 of
+            True  -> Just "buzz"
+            False -> Nothing
+
+fizzBuzz :: Int -> Maybe String
+fizzBuzz x = case x `divisable` 15 of
+                True  -> Just "fizzbuzz"
+                False -> Nothing
+
+none :: Int -> Maybe String
+none x = Just (show x)
+
+unwrap :: Maybe String -> String
+unwrap (Just x) = x
 
 divisable :: Int -> Int -> Bool
 divisable x y = x `mod` y == 0
