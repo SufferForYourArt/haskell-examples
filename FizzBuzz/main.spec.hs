fc14f69e32d58ebf76641a3ae0051cd9f1165df1
Kurt Logan <kurt.logan@digital.hmrc.gov.uk>
2017-05-18 10:52:32 +0100

    Refactored variable names

--- FizzBuzz/main.spec.hs
+++ FizzBuzz/main.spec.hs
@@ -3,27 +3,27 @@ module Test where
 import App
 import Test.QuickCheck
 
-f = "fizz"
-b = "buzz"
-fb = f ++ b
+fizz = "fizz"
+buzz = "buzz"
+fizzbuzz = fizz ++ buzz
 
 prop_fizzleMaBizzle :: Int -> Bool
 prop_fizzleMaBizzle x = case fizzleMaBizzle x of
-                        "fizz"     -> fizzleMaBizzle (x * 3) == f &&
-                                      fizzleMaBizzle (x * 5) == fb &&
-                                      fizzleMaBizzle (x * 3 * 5) == fb
+                        "fizz"     -> fizzleMaBizzle (x * 3) == fizz &&
+                                      fizzleMaBizzle (x * 5) == fizzbuzz &&
+                                      fizzleMaBizzle (x * 3 * 5) == fizzbuzz
 
-                        "buzz"     -> fizzleMaBizzle (x * 3) == fb &&
-                                      fizzleMaBizzle (x * 5) == b &&
-                                      fizzleMaBizzle (x * 3 * 5) == fb
+                        "buzz"     -> fizzleMaBizzle (x * 3) == fizzbuzz &&
+                                      fizzleMaBizzle (x * 5) == buzz &&
+                                      fizzleMaBizzle (x * 3 * 5) == fizzbuzz
 
-                        "fizzbuzz" -> fizzleMaBizzle (x * 3) == fb &&
-                                      fizzleMaBizzle (x * 5) == fb &&
-                                      fizzleMaBizzle (x * 3 * 5) == fb
+                        "fizzbuzz" -> fizzleMaBizzle (x * 3) == fizzbuzz &&
+                                      fizzleMaBizzle (x * 5) == fizzbuzz &&
+                                      fizzleMaBizzle (x * 3 * 5) == fizzbuzz
 
                         result     -> (read result :: Int) == x &&
-                                      fizzleMaBizzle (x * 3) == f &&
-                                      fizzleMaBizzle (x * 5) == b &&
-                                      fizzleMaBizzle (x * 3 * 5) == fb
+                                      fizzleMaBizzle (x * 3) == fizz &&
+                                      fizzleMaBizzle (x * 5) == buzz &&
+                                      fizzleMaBizzle (x * 3 * 5) == fizzbuzz
 
 test = quickCheck prop_fizzleMaBizzle
